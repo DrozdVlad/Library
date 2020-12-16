6c3e65420c787d229bcc174add971ad0ade2a15e
DrozdVlad <vlad1995d@gmail.com>
2020-12-16 23:43:08 +0200

    djando mozila home work done

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
+from rest_framework import status
+from rest_framework.response import Response
+
 from .models import Book, Author, BookInstance, Genre
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -46,3 +50,13 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+class LoanedBooksByAllUsersListView(LoginRequiredMixin, generic.ListView):
+    model = BookInstance
+    permission_required = 'catalog.can_mark_returned'
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.all()
